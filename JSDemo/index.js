7e84f8ea6deb4d68fa104c84ad833ab345345827
sivaz020156 <sivaprakash.cta.cit@gmail.com>
2020-10-13 09:50:29 +0530

    Java script and Html world

---
+++ JSDemo/index.js
@@ -0,0 +1,10 @@
+
+import {cal} from './caculator.js';
+
+//alert("This is Es6 Java script");
+console.log('this is console to debug Arun');
+console.log('This is add  '+cal.add(2,2));
+console.log('This is subtract '+cal.sub(10,5));
+//mutliply
+
+//divison
